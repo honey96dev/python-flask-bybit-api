6b96e0a7156b2fdc90b60a4246b9c4f5ef460c79
sun1earth1moon1 <honey96dev@gmail.com>
2019-04-11 12:33:53 +0000

    now order & stop-order finished

--- app.py
+++ app.py
@@ -89,6 +89,26 @@ def order_list():
     #     return 'error'
 
 
+@app.route('/order/cancel', methods=['POST'])
+def order_cancel():
+    return orders.order_cancel()
+
+
+@app.route('/stop-order/create', methods=['POST'])
+def stop_order_create():
+    return orders.stop_order_create()
+
+
+@app.route('/stop-order/list', methods=['GET'])
+def stop_order_list():
+    return orders.stop_order_list()
+
+
+@app.route('/stop-order/cancel', methods=['POST'])
+def stop_order_cancel():
+    return orders.stop_order_cancel()
+
+
 if __name__ == '__main__':
     context = ('domain.crt', 'domain.key')
     # app.run(host='0.0.0.0', port=443, ssl_context='adhoc')
--- orders.py
+++ orders.py
@@ -89,3 +89,151 @@ def order_list():
         return json.dumps(
             {'ret_msg': 'error'}
         )
+
+
+def order_cancel():
+    params = request.args
+    order_id = params.get('order_id')
+    timestamp = get_current_timestamp()
+
+    params = {
+        'order_id': order_id,
+        'timestamp': timestamp,
+    }
+    params_string = generate_params_string(params)
+    sign = generate_hmac_sha256_hex(secret_key, params_string)
+
+    url = '{}/open-api/order/cancel'.format(server_base_url)
+
+    params['api_key'] = api_key
+    params['sign'] = sign
+
+    try:
+        r = requests.post(url=url, params=params)
+
+        formatted_string = r.text.replace("'", '"')
+        rows = json.loads(formatted_string)
+
+        return json.dumps(rows)
+    except:
+        return json.dumps(
+            {'ret_msg': 'error'}
+        )
+
+
+def stop_order_create():
+    params = request.args
+    base_price = params.get('base_price')
+    order_link_id = params.get('order_link_id')
+    order_type = params.get('order_type')
+    price = params.get('price')
+    qty = params.get('qty')
+    side = params.get('side')
+    symbol = params.get('symbol')
+    stop_px = params.get('stop_px')
+    time_in_force = params.get('time_in_force')
+    timestamp = get_current_timestamp()
+
+    params = {
+        'base_price': base_price,
+        'order_link_id': order_link_id,
+        'order_type': order_type,
+        'price': price,
+        'qty': qty,
+        'side': side,
+        'stop_px': stop_px,
+        'symbol': symbol,
+        'time_in_force': time_in_force,
+        'timestamp': timestamp,
+    }
+    params_string = generate_params_string(params)
+    sign = generate_hmac_sha256_hex(secret_key, params_string)
+
+    url = '{}/open-api/stop-order/create'.format(server_base_url)
+
+    params['api_key'] = api_key
+    params['sign'] = sign
+
+    try:
+        r = requests.post(url=url, params=params)
+
+        formatted_string = r.text.replace("'", '"')
+        rows = json.loads(formatted_string)
+
+        return json.dumps(rows)
+    except:
+        return json.dumps(
+            {'ret_msg': 'error'}
+        )
+
+
+def stop_order_list():
+    params = request.args
+    limit = params.get('limit')
+    order = params.get('order')
+    order_link_id = params.get('order_link_id')
+    page = params.get('page')
+    sort = params.get('sort')
+    stop_order_id = params.get('stop_order_id')
+    symbol = params.get('symbol')
+    timestamp = get_current_timestamp()
+
+    params = {
+        'limit': limit,
+        'order': order,
+        'order_link_id': order_link_id,
+        'page': page,
+        'sort': sort,
+        'stop_order_id': stop_order_id,
+        'symbol': symbol,
+        'timestamp': timestamp,
+    }
+    params_string = generate_params_string(params)
+    sign = generate_hmac_sha256_hex(secret_key, params_string)
+
+    url = '{}/open-api/stop-order/list'.format(server_base_url)
+
+    params['api_key'] = api_key
+    params['sign'] = sign
+
+    try:
+        r = requests.get(url=url, params=params)
+
+        formatted_string = r.text.replace("'", '"')
+        rows = json.loads(formatted_string)
+
+        return json.dumps(rows)
+    except:
+        return json.dumps(
+            {'ret_msg': 'error'}
+        )
+
+
+def stop_order_cancel():
+    params = request.args
+    stop_order_id = params.get('stop_order_id')
+    timestamp = get_current_timestamp()
+
+    params = {
+        'stop_order_id': stop_order_id,
+        'timestamp': timestamp,
+    }
+    params_string = generate_params_string(params)
+    sign = generate_hmac_sha256_hex(secret_key, params_string)
+
+    url = '{}/open-api/stop-order/cancel'.format(server_base_url)
+
+    params['api_key'] = api_key
+    params['sign'] = sign
+
+    try:
+        r = requests.post(url=url, params=params)
+
+        formatted_string = r.text.replace("'", '"')
+        rows = json.loads(formatted_string)
+
+        return json.dumps(rows)
+    except:
+        return json.dumps(
+            {'ret_msg': 'error'}
+        )
